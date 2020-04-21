e64ac95620b30deca9e4fd69c03ccad7b4ed8c62
Nicola Sorace <sorace.n@gmail.com>
2020-04-21 13:18:00 +0100

    Loads of changes and additions

--- quiz.py
+++ quiz.py
@@ -5,6 +5,7 @@ import hashlib, time
 app = Flask(__name__)
 
 DATABASE = 'quiz.db'
+SECRET_ADMIN_NAME = '_secadmin'
 
 def get_db():
     db = getattr(g, '_database', None)
@@ -35,22 +36,23 @@ def close_connection(exception):
 def quiz_view():
     db = get_db()
     cur = db.cursor()
+    now_time = int(time.time())
 
     name = session.get('name', '')
     player = cur.execute('SELECT * FROM players WHERE name=?', (name,)).fetchone()
     if player == None:
-        # return 'Please <a href="/login">Login again</a>'
         return 'NOLOGIN'
     else:
         cur.execute('UPDATE players SET last_seen=? WHERE name=?', (int(time.time()), name))
+        db.commit()
     
     state = cur.execute('SELECT * FROM state').fetchone()
-    players = cur.execute('SELECT * FROM players').fetchall()
+    players = cur.execute('SELECT * FROM players WHERE name!=?', (SECRET_ADMIN_NAME,)).fetchall()
     r_num = state['r_num']
     q_num = state['q_num']
 
     if r_num == 0: # Quiz not started
-        return render_template('quiz_not_started.html', players=players)
+        return render_template('quiz_not_started.html', players=players, name=name, now_time=now_time)
 
     questions = cur.execute('SELECT * FROM questions WHERE r_num=? AND q_num<=? ORDER BY r_num, q_num ASC', (r_num, q_num)).fetchall()
     responses = cur.execute('SELECT * FROM responses WHERE r_num=? AND name=?', (r_num, name)).fetchall()
@@ -59,7 +61,7 @@ def quiz_view():
         answers = {}
         for r in responses:
             answers[r['q_num']] = r['answer']
-        return render_template('quiz_round.html', players=players, r_num=r_num, questions=questions, answers=answers, enumerate=enumerate)
+        return render_template('quiz_round.html', players=players, name=name, now_time=now_time, r_num=r_num, questions=questions, answers=answers, enumerate=enumerate)
     else: # Completed round
         responses = cur.execute('SELECT * FROM responses WHERE r_num=?', (r_num,)).fetchall()
         org_responses = {} # Group responses into dictionary by question number
@@ -69,13 +71,17 @@ def quiz_view():
                 org_responses[q] = [response]
             else:
                 org_responses[q].append(response)
-        return render_template('quiz_round_done.html', players=players, r_num=r_num, questions=questions, responses=org_responses, enumerate=enumerate)
+        return render_template('quiz_round_done.html', players=players, name=name, now_time=now_time, r_num=r_num, questions=questions, responses=org_responses, enumerate=enumerate)
 
 @app.route('/quiz_endpoint', methods=['POST'])
 def quiz_endpoint():
     name = session.get('name', '')
+
     if name == '':
         return abort(403)
+    if name == SECRET_ADMIN_NAME:
+        return 'ok'
+
     db = get_db()
     cur = db.cursor()
     r_num = cur.execute('SELECT r_num FROM state').fetchone()[0]
@@ -152,7 +158,7 @@ def login():
     if 'name' in request.form:
         session['name'] = name = request.form['name']
         db = get_db()
-        db.cursor().execute('INSERT INTO players (name, score, last_seen) VALUES (?, 0, ?)', (name, int(time.time())))
+        db.cursor().execute('INSERT INTO players (name, score, admin, last_seen) VALUES (?, 0, ?, ?)', (name, int(name==SECRET_ADMIN_NAME), int(time.time())))
         db.commit()
         return redirect('/', code=302)
     return render_template('login.html')
